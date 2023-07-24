ae30270039d3787f3206bc90ad2db5c16a51b6f8
Justin Shams <justinshams@gmail.com>
2023-07-24 16:00:49 -0700

    Adding header, nav, main, and footer components

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import logo from "../assets/little-lemon-large.png"
+
+const Header = (
+    <header>
+        <img src={logo}></img>
+    </header>
+)
+
+
+export default Header
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import Logosm from "../assets/Logo.svg"
+const Nav = (
+    <nav>
+        <ul>
+            <img src={Logosm}/>
+            <li><a href="/#home">Home</a></li>
+            <li><a href="/#about">About</a></li>
+            <li><a href="/#menu">Menu</a></li>
+            <li><a href="/#reservtions">Reservations</a></li>
+            <li><a href="/#order-online">Order Online</a></li>
+            <li><a href="/#login">Login</a></li>
+        </ul>
+    </nav>
+)
+
+export default Nav
